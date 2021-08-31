8ec22926a02fce32d2008695fd3f42def9314006
Codeacula <codeacula@codeacula.com>
2021-08-30 21:32:48 -0500

    Basic styling stuff is up, also using pug now

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+<template lang="pug">
+.app-wrapper
+  nav.navigation
+    router-link(:to="{ name: 'Home' }") Home
+    router-link(:to="{ name: 'About' }") About
+  router-view.main-content
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./sass/main.scss";
+
+.app-wrapper {
+  display: grid;
+  grid-template-rows: [row-start navigation-start] 1.5rem [navigation-end content-start] auto [content-end row-end];
+  height: 100%;
+  width: 100%;
+
+  & > .main-content {
+    display: flex;
+    height: 100%;
+    grid-row: content-start / content-end;
+    width: 100%;
+  }
+
+  & > .navigation {
+    grid-row: navigation-start / navigation-end;
+    height: 100%;
+    width: 100%;
+  }
 }
 
-#nav {
-  padding: 30px;
+.navigation {
+  background-color: $dark-3;
+  border-bottom: solid 1px $dark-6;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    display: inline-block;
+    padding: 0 1rem;
+    text-decoration: none;
+    transition: all 0.1s ease-in;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &:hover {
+      background-color: $dark-2;
+      color: $gold;
     }
   }
 }
