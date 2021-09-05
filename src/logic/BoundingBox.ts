3373cbb63371b0972aa87636e5b178cb1a536203
Codeacula <codeacula@codeacula.com>
2021-09-05 15:14:10 -0500

    Can now place items and see them on the screen

---
+++ src/logic/BoundingBox.ts
@@ -0,0 +1,23 @@
+import { Vec3 } from '.';
+
+export class BoundingBox {
+  constructor(from: Vec3, to: Vec3) {
+    this.from = from;
+    this.to = to;
+  }
+
+  private from: Vec3;
+  private to: Vec3;
+
+  public contains(point: Vec3): boolean {
+    return point.x >= this.from.x && point.x <= this.to.x && point.y >= this.from.y && point.y < this.to.y;
+  }
+
+  public static fromSize(origin: Vec3, size: Vec3): BoundingBox {
+    return new BoundingBox(origin, new Vec3(origin.x + size.x, origin.y + size.y));
+  }
+
+  public touches(boundingBox: BoundingBox): boolean {
+    return boundingBox.contains(this.from) || boundingBox.contains(this.to);
+  }
+}
