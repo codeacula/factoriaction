cff750e58e39d81864ccae8270edd894024dec03
Codeacula <codeacula@codeacula.com>
2021-09-06 12:25:19 -0500

    Removing rule comment from GridRenderer, swapping which no var rule we squelch

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 0,
-    '@typescript-eslint/no-unused-vars': [2, { args: 'after-used', argsIgnorePattern: '^_' }],
+    'no-unused-vars': [2, { args: 'after-used', argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': 0,
     quotes: ['error', 'single'],
   },
   overrides: [
