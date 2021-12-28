8899e94bc79bcb66cc013d0dcf740ce36f1bf318
Codeacula <codeacula@codeacula.com>
2021-12-28 10:04:43 -0600

    Still working on jest and docker, but it's FF14 time

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,15 +1,20 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: 'ts-jest',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.vue'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/src/main.ts', '<rootDir>/src/registerServiceWorker.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/main.ts'],
   coverageThreshold: {
     global: {},
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFiles: ['jest-canvas-mock'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
+  testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
 };
